92e9c3abe8baec72a6e534f870e232c4befb48e1
Ali1en131 <essanurcahyania@gmail.com>
2024-03-16 07:41:17 +0700

    update web

--- routes/web.php
+++ routes/web.php
@@ -18,6 +18,10 @@ use Illuminate\Support\Facades\Route;
 
 
 
+Route::get('/', function () {
+    return view('welcome');
+});
+
 
 Route::get('/dashboard', function () {
     return view('dashboard');
